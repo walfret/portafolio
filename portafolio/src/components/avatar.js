2eff709037ef50999f5866418e99e785586eda9c
Walfret Rodriguez <walfret69@gmail.com>
2020-12-26 17:50:55 -0600

    Proyecto completado

--- portafolio/src/components/avatar.js
+++ portafolio/src/components/avatar.js
@@ -1,49 +1,72 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './styles/avatar.css';
-import logo from '../logo.svg'
-import AvatarNew from './avatarNew';
+import "./styles/avatar.css";
+import logo from "../logo.svg";
+import AvatarNew from "./avatarNew";
 
 class avatar extends React.Component {
-    render () {
-        return (
-            <React.Fragment>
-               <div id="inicio" className='header'>
-                    <div>
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <p className="cr">CR</p>
-                    </div>
-                    <div className="menu">
-                        <Link className='nav-words' to="/">Home</Link>
-                        <Link className='nav-words' to="/me">Mas acerca</Link>
-                    </div>
-                    <div>
-                        <a 
-                        href="https://www.facebook.com/walfret.bravo" 
-                        className='social-link facebook'>
-                        </a>
-                        <a 
-                        href="https://www.instagram.com/walfret/" 
-                        className='social-link instagram'>
-                        </a>
-                        <a 
-                        href="https://github.com/walfret?tab=repositories" 
-                        className='social-link github'>
-                        </a>
-                        <a 
-                        href="https://api.whatsapp.com/send/?phone=522722143009&text&app_absent=0" 
-                        className='social-link whats'>
-                        </a>
-                        <i id="burger-menu" className="burger-button" onClick={this.handleClick}></i>
-                    </div>
-                </div>
-                <div className="page-avatar">
-                    <AvatarNew />
-                </div>
-            </React.Fragment>
-        )
-    }
+  state = {
+    show: false,
+  };
+
+  handleClick = () => {
+    this.setState({
+      show: !this.state.show,
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div id="inicio" className="header">
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p className="cr">CR</p>
+          </div>
+          <div
+            className="menu"
+            style={{ bottom: this.state.show === true ? 0 : "-250vw" }}
+          >
+            <nav>
+              <Link className="nav-words" to="/">
+                Home
+              </Link>
+              <Link className="nav-words" to="/me">
+                Mas acerca
+              </Link>
+            </nav>
+          </div>
+          <div>
+            <a
+              href="https://www.facebook.com/walfret.bravo"
+              className="social-link facebook"
+            ></a>
+            <a
+              href="https://www.instagram.com/walfret/"
+              className="social-link instagram"
+            ></a>
+            <a
+              href="https://github.com/walfret?tab=repositories"
+              className="social-link github"
+            ></a>
+            <a
+              href="https://api.whatsapp.com/send/?phone=522722143009&text&app_absent=0"
+              className="social-link whats"
+            ></a>
+            <i
+              id="burger-menu"
+              className="burger-button"
+              onClick={this.handleClick}
+            ></i>
+          </div>
+        </div>
+        <div className="page-avatar">
+          <AvatarNew />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default avatar;
--- portafolio/src/components/avatarNew.js
+++ portafolio/src/components/avatarNew.js
@@ -33,27 +33,25 @@ class avatarNew extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div>
+        <div className="container-avatarPages">
           <div>
-            <div>
-              <AvatarBadge
-                firstName={this.state.form.firstName || 'FIRST_NAME'}
-                lastName={this.state.form.lastName || 'LAST_NAME'}
-                twitter={this.state.form.twitter || 'twitter'}
-                jobTitle={this.state.form.jobTitle || 'JOB_TITLE'}
-                email={this.state.form.email || 'EMAIL'}
-                avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
-              />
-            </div>
-            <div>
-              <h1>Datos Personales</h1>
-              <AvatarForm
-                onChange={this.handleChange}
-                onSubmit={this.handleSubmit}
-                formValues={this.state.form}
-                error={this.state.error}
-              />
-            </div>
+            <AvatarBadge
+              firstName={this.state.form.firstName || 'FIRST_NAME'}
+              lastName={this.state.form.lastName || 'LAST_NAME'}
+              twitter={this.state.form.twitter || 'twitter'}
+              jobTitle={this.state.form.jobTitle || 'JOB_TITLE'}
+              email={this.state.form.email || 'EMAIL'}
+              avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+            />
+          </div>
+          <div>
+            <h1>Datos Personales</h1>
+            <AvatarForm
+              onChange={this.handleChange}
+              onSubmit={this.handleSubmit}
+              formValues={this.state.form}
+              error={this.state.error}
+            />
           </div>
         </div>
       </React.Fragment>
