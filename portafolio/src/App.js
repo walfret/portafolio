3298043c4fa28a8c59166d84d1191fac04a61972
Walfret Rodriguez <walfret69@gmail.com>
2021-02-09 15:28:50 -0600

    Corrigiendo cositas

--- portafolio/src/App.js
+++ portafolio/src/App.js
@@ -3,12 +3,8 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import Home from "./components/home";
 import Layout from "./components/layout";
-import Header from "./components/header";
 import Api from "./components/juego";
 import Avatar from "./components/avatar";
-import About from "./components/about";
-import Technologies from "./components/technologies";
-import Contact from "./components/contact";
 import NotFound from "./components/notFound";
 
 class App extends React.Component {
@@ -22,10 +18,6 @@ class App extends React.Component {
             <Route exact path="/me" component={Layout} />
             <Route exact path="/api" component={Api} />
             <Route exact path="/me/avatar" component={Avatar} />
-            <Route exact path="/me/inicio" component={Header} />
-            <Route exact path="/me/acerca" component={About} />
-            <Route exact path="/me/tecnologias" component={Technologies} />
-            <Route exact path="/me/contacto" component={Contact} />
             <Route component={NotFound} />
           </Switch>
         </React.Fragment>
